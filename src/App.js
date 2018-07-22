95356c52c73cc628d64e509dd2a59d3bda809f51
Hanseong Park <hs88610@gmail.com>
2018-07-22 17:02:31 +0900

    fetch, ajax

--- src/App.js
+++ src/App.js
@@ -5,42 +5,10 @@ import Movie from './Movie.js';
 
 class App extends Component {
   
-  state = {
-  }
-  
+  state = {}
+
   componentDidMount(){
-    //기존 function 선언
-    // setTimeout(function(){
-      //   console.log("");
-      // }, 1000}
-      
-      //최신 자바스크립트 () => {}
-      setTimeout(() => {
-        this.setState({
-            movies : [
-              {
-                title : "matirx",
-                poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
-              },
-              {
-                title : "full metal",
-                poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
-              },
-              {
-                title : "oldboy",
-                poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
-              },
-              {
-                title : "dokkk",
-                poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
-              },
-              {
-                title:"Transporting",
-                poster : "https://cdn2.i-scmp.com/sites/default/files/styles/landscape/public/images/methode/2017/02/16/36445594-edd4-11e6-8960-2c6b8565de23_1280x720.jpg?itok=N9YiRUyJ"
-              }
-            ]
-      })
-    }, 5000)
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'));   
   }
   
   _renderMovies = () => {
