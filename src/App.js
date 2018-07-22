1dd51a7c67abbab8440179103fd582b9aed96b38
Hanseong Park <hs88610@gmail.com>
2018-07-22 12:06:57 +0900

    arrow function

--- src/App.js
+++ src/App.js
@@ -3,45 +3,46 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie.js';
 
-const movies = [
-  {
-    title : "matirx",
-    poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
-  },
-  {
-    title : "full metal",
-    poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
-  },
-  {
-    title : "oldboy",
-    poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
-  },
-  {
-    title : "dokkk",
-    poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
-  }
-]
-
-
 class App extends Component {
   
   state = {
-    greeting: "Hello!"
+    movies : [
+      {
+        title : "matirx",
+        poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
+      },
+      {
+        title : "full metal",
+        poster : "https://static.vrv.co/imgsrv/display/thumbnail/240x360/catalog/crunchyroll/0662921aa3b81ff85737ddeb56deefab.jpg"
+      },
+      {
+        title : "oldboy",
+        poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
+      },
+      {
+        title : "dokkk",
+        poster : "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg"
+      }
+    ]
   }
 
   componentDidMount(){
+    //기존 function 선언
+    // setTimeout(function(){
+    //   console.log("");
+    // }, 1000}
+
+    //최신 자바스크립트 () => {}
     setTimeout(() => {
-      this.setState({
-        greeting : 'Hello again!'
+    this.setState({
       })
-    }, 5000)
+    }, 1000)
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map( (movie, index) => {
+        {this.state.movies.map( (movie, index) => {
            return < Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
       </div>
