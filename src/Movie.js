c09345586d1f31ef66dd63dcfae209a9959e0bed
Hanseong Park <hs88610@gmail.com>
2018-07-22 15:56:06 +0900

    dump(멍청한), smart component 차이

--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 import propTypes from 'prop-types';
 
-class Movie extends Component{
+// class Movie extends Component{
 
-    static propTypes = {
-        title : propTypes.string.isRequired,
-        poster : propTypes.string.isRequired
-    }
+//     static propTypes = {
+//         title : propTypes.string.isRequired,
+//         poster : propTypes.string.isRequired
+//     }
 
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>     
-            </div>
-        )
-    }
+//     render(){
+//         return(
+//             <div>
+//                 <MoviePoster poster={this.props.poster}/>
+//                 <h1>{this.props.title}</h1>     
+//             </div>
+//         )
+//     }
+// }
+
+// class MoviePoster extends Component{
+    
+//     static propTypes ={
+//         poster : propTypes.string.isRequired
+//     }
+
+//     render(){
+//         return(
+//             <img src={this.props.poster} alt="Movie Poster"/>
+//         )
+//     }
+// }
+
+//dump component
+function Movie({poster, title}){
+    return (
+        <div>
+            <MoviePoster poster={poster}/>
+            <h1>{title}</h1>     
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-    
-    static propTypes ={
-        poster : propTypes.string.isRequired
-    }
+Movie.prototype = {
+    poster : propTypes.string.isRequired,
+    title : propTypes.string.isRequired
+}
 
-    render(){
-        return(
-            <img src={this.props.poster} alt="Movie Poster"/>
-        )
-    }
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster"/>
+    )
+}
+
+MoviePoster.propTypes = {
+    poster : propTypes.string.isRequired
 }
 
 export default Movie
