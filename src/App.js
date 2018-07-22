530c4d929541504e8e3de2982252fbe65d6507e7
Hanseong Park <hs88610@gmail.com>
2018-07-22 11:57:47 +0900

    state 변경

--- src/App.js
+++ src/App.js
@@ -22,12 +22,25 @@ const movies = [
   }
 ]
 
-console.log("11");
 
 class App extends Component {
+  
+  state = {
+    greeting: "Hello!"
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        greeting : 'Hello again!'
+      })
+    }, 5000)
+  }
+
   render() {
     return (
       <div className="App">
+        {this.state.greeting}
         {movies.map( (movie, index) => {
            return < Movie title={movie.title} poster={movie.poster} key={index}/>
         })}
